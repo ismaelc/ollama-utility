b9202e2df492186d8246cc958459850c4fe7a684
Chris Ismael <chris.ismael@gmail.com>
2024-03-17 23:22:42 -0700

    Update sample sessions/prompts

--- tools/search_web.py
+++ tools/search_web.py
@@ -2,13 +2,9 @@ from duckduckgo_search import ddg
 from newspaper import Article
 # import json
 
-
 def search_web(query):
     results = ddg(query, max_results=3)
     for result in results:
-        if 'body' in result:
-            result['text'] = result['body']
-            del result['body']
         if result['href']:
             try:
                 article = Article(result['href'])
@@ -20,5 +16,9 @@ def search_web(query):
                     result['text'] = article.text
             except Exception as e:
                 result['error'] = str(e)
+            finally:
+                result['text'] = result.get('text', result.get('body', ''))
+                if 'body' in result:
+                    del result['body']
     # print(json.dumps(results, indent=2))
     return results
